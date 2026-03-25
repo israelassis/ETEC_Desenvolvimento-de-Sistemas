cecabc3655f7850549beb2670c3468c25ad91a10
Rael_Tech <assis.phn@gmail.com>
2026-03-25 14:41:41 -0300

    Aula Desenvolvimento de Sistema 1 - Consumo de Energia

---
+++ mod1_DS-1/consumo-energia.py
@@ -0,0 +1,23 @@
+#parametro
+valor_kwh = 0.75
+calculo_mes = 30 #mes comercial
+
+print("\n-- Calculadora de Consumo Energético --")
+
+#entrada de dados
+nome_aparelho = input("Nome do aparelho (ex: Geladeira): ")
+potencia = float(input(f"Potência de {nome_aparelho} em Watts (W): "))
+horas_dia = float(input(f"Horas de uso por dia de {nome_aparelho} (ex: 2 horas e meia = 2.5 ou 2 horas = 2) "))
+
+
+#processamento
+# Fórmula: (Watts * horas * 30 dias) / 1000 para converter em kWh
+consumo_mensal = (potencia * horas_dia * 30) / 1000
+custo_estimado = consumo_mensal * valor_kwh
+
+#exibição
+print("-" * 40)
+print(f"APARELHO: {nome_aparelho.upper()}")
+print(f"CONSUMO ESTIMADO: {consumo_mensal:.2f} KWh/mês")
+print(f"CUSTO ESTIMADO: R$ {custo_estimado:.2f}/mês")
+print("-" * 40)
