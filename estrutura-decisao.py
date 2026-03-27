75c8f61ef3f6b63f76e5d2cb1a6c76edc2a5d666
Rael_Tech <assis.phn@gmail.com>
2026-03-27 10:13:36 -0300

    feat: estrutura de decisão - if/elif/else

---
+++ estrutura-decisao.py
@@ -0,0 +1,51 @@
+"""
+Todos os dias, somos confrontados com escolhas que parecem simples, mas
+carregam consigo consequências importantes na nossa rotina e no modo como
+encaramos o mundo!
+"""
+
+# estrutura condicional simples
+# “Se isso acontecer, faça aquilo.”
+
+idade = int(input("Qual sua idade: "))
+if idade >= 18:
+    print("Maior de idade")
+
+
+temperatura = float(input("Digite a temperatura: "))
+"""O programa verifica a condição (temperatura > 30). 
+    Se essa condição for verdadeira (True), ele executa o comando que está dentro do if. 
+    Caso seja falsa (False), o programa simplesmente continua sua execução normalmente, 
+    sem executar nenhuma ação adicional."""
+
+if temperatura > 30:
+ print("Está muito quente hoje!")
+
+
+# estrutura condicional COMPOSTA
+# “Se isso acontecer, faça isso.
+# Senão, se for aquilo acontecer, faça aquilo”
+valor_compra = float(input("Digite o valor da compra: R$ "))
+"""A condição (valor_compra >= 100) é avaliada pelo programa. 
+    Se for verdadeira, ele exibe a mensagem informando que o cliente tem direito ao desconto. 
+    Caso seja falsa, o bloco do else é executado, informando que não há desconto disponível"""
+
+if valor_compra >= 100:
+ print("Você ganhou um desconto de 10%!")
+else:
+ print("Sem desconto. Aproveite as próximas promoções!") 
+
+
+# estrutura condicional ENCADEADA - "ANINHADA"
+# "Se for isso, faça isso. Senão, se for aquilo,
+# faça aquilo. Caso contrário, faça outra coisa.”
+
+nota = float(input("Digite sua nota final: "))
+if nota >= 9:
+    print("Excelente desempenho!")
+elif nota >= 7:
+    print("Aprovado!")
+elif nota >= 5:
+    print("Recuperação.")
+else:
+    print("Reprovado.")
